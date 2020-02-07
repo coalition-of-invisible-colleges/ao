8dac220750170886b1dc07256861ddb0933536bf
Anders Aamodt <anders.aamodt@gmail.com>
2020-02-07 02:12:38 -0700

    memes load and hashes output to console on startup

--- src/utils/filesharing.js
+++ src/utils/filesharing.js
@@ -1,4 +1,6 @@
 const ipfsClient = require('ipfs-http-client')
+const { globSource } = ipfsClient
+const BufferList = require('bl/BufferList')
 
 let ipfs
 
@@ -7,26 +9,41 @@ function initialize() {
 	ipfs = ipfsClient('http://localhost:5001')
 }
 
-function scanMemes() {
+async function scanMemes() {
 	try {
 		console.log('scanning memes')
-		let promise = ipfs.addFromFs('../.ao/memes', { recursive: true })
-		console.log("promise is", promise)
-		Promise.all([promise]).then((result) => {
-			console.log("result2 is", result)
-			console.log("result length is ", result[0].length)
-			let hash = result[0][result.length - 1].hash
-			Promise.all([hash]).then((hashresult) => {
-				let files = ipfs.get(hashresult)
-				Promise.all([files]).then((filesresult) => {
-					let pin = ipfs.pin.add(hashresult)
-					Promise.all([pin]).then((pinresult) => {
-						console.log("all files are")
-						console.log(filesresult)
-					})
-				})
-			})
-		})
+		// let promise = ipfs.add(globSource('../.ao/memes', { recursive: true }))
+		let hash
+		for await (const file of ipfs.add(globSource('../.ao/memes', { recursive: true }))) {
+			console.log("scanning meme")
+			hash = file.cid
+			console.log("meme scanned: ", String(file.cid))
+		}
+		// console.log("after scan memes")
+		// console.log("CID is ", hash)
+		// for await (const file of ipfs.get(hash)) {
+		// 	console.log(file.path)
+		// 	// let loadedfile = ipfs.get(file.path)
+		// 	const content = new BufferList()
+		// 	for await (const chunk of file.content) {
+		// 		content.append(chunk)
+		// 	}
+
+		// 	console.log(content.toString())
+		// }
+		// console.log("all files are")
+		// console.log(loaded)
+		// console.log("promise is", promise)
+		// Promise.all([promise]).then((result) => {
+		// 	console.log("result2 is", result)
+		// 	console.log("result length is ", result[0].length)
+		// 	Promise.all([hash]).then((hashresult) => {
+		// 		Promise.all([files]).then((filesresult) => {
+		// 			Promise.all([pin]).then((pinresult) => {
+		// 			})
+		// 		})
+		// 	})
+		// })
 	} catch(err) {
 		console.log("scanning memes failed. error: ", err)
 	}
